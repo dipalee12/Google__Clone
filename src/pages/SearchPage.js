534ee07e2055496409c6cec35f60bb67bd96afa4
Dipalee Chauhan <75933503+dipalee12@users.noreply.github.com>
2024-02-22 14:04:32 +0530

    Update SearchPage.js

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -83,7 +83,7 @@ function SearchPage() {
 
           {data?.items.map(item =>(
             <div className="searchPage__result" >
-              <a className="searchPage_resultLink" href="{item.link}">
+              <a className="searchPage_resultLink" href={item.link}>
               {item.pagemap?.cse_image?.length > 0 &&  item.pagemap?.cse_image[0]?.src && (
                   <img className="searchPage__resultImage" src={
                     item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src
